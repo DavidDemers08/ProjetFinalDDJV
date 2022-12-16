d469fb82766449c41adbd0513933c93b0a0393d4
Carlens Belony <carlens2000@live.ca>
2022-12-16 17:13:39 -0500

    menu presque fini

---
+++ Assets/titrescript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class titrescript : MonoBehaviour
+{
+    private int multiplier = -1;
+    private float distance = 1;
+    private float compteur = 0;
+
+
+    void Start()
+    {
+
+        //StartCoroutine(CoroutineAnim());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+        //IEnumerator CoroutineAnim()
+    //{
+        
+       // if (compteur >= 15) { 
+       //     distance = -distance;
+      //      compteur = 0;
+      //  }
+
+       // this.gameObject.transform.position.y += distance;
+       // compteur += 1;
+
+
+   // }
+}
